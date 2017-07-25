39eb0d6320219c9020cbe4bd4b060d6e154a9c36
Ray Assch <reinier_a@hotmail.com>
2017-07-25 08:52:13 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -4,14 +4,14 @@ import lxml.html
 
 # scrape_table function: gets passed an individual page to scrape
 def scrape_table(root):
-    rows = root.cssselect("table.1 tr")  # selects all <tr> blocks within <table class="Trolley table">
+    rows = root.cssselect("table.wikitable tr")  # selects all <tr> blocks within <table class="Trolley table">
     for row in rows:
         # Set up our data record - we'll need it later
         record = {}
         table_cells = row.cssselect("p")
         if table_cells: 
-            record['Racecourse'] = table_cells[0].text_content()
-            record['Address and Phone Number'] = table_cells[1].text_content()
+            record['Phones'] = table_cells[0].text_content()
+            record['Examples'] = table_cells[1].text_content()
             #record['Region'] = table_cells[2].text_content()
             #record['Trolley total'] = table_cells[3].text_content()
             #record['Sales m'] = table_cells[4].text_content()
@@ -19,7 +19,7 @@ def scrape_table(root):
             # Print out the data we've gathered
             print record, '------------'
             # Finally, save the record to the datastore - 'Artist' is our unique key
-            scraperwiki.sqlite.save(["Racecourse"], record)
+            scraperwiki.sqlite.save(["Phones"], record)
                             
 # scrape_and_look_for_next_link function: calls the scrape_table
 # function, then hunts for a 'next' link: if one is found, calls itself again
@@ -40,6 +40,6 @@ def scrape_and_look_for_next_link(url):
 # START HERE: define your starting URL - then 
 # call a function to scrape the first page in the series.
 # ---------------------------------------------------------------------------
-#base_url = 'http://www.ukjockey.com/racecourses.html'
-starting_url = 'http://www.ukjockey.com/racecourses.html'
+#base_url = 'https://en.wikipedia.org/wiki/International_Phonetic_Alphabet_chart_for_English_dialects'
+starting_url = 'https://en.wikipedia.org/wiki/International_Phonetic_Alphabet_chart_for_English_dialects'
 scrape_and_look_for_next_link(starting_url)
