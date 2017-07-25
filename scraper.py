24bf3d76fb6d56d63af2f44e8c499e7522b0505d
Ray Assch <reinier_a@hotmail.com>
2017-07-25 08:39:54 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -4,22 +4,22 @@ import lxml.html
 
 # scrape_table function: gets passed an individual page to scrape
 def scrape_table(root):
-    rows = root.cssselect("table.Trolley.table tr")  # selects all <tr> blocks within <table class="Trolley table">
+    rows = root.cssselect("table.1 tr")  # selects all <tr> blocks within <table class="Trolley table">
     for row in rows:
         # Set up our data record - we'll need it later
         record = {}
         table_cells = row.cssselect("td")
         if table_cells: 
-            record['Date'] = table_cells[0].text_content()
-            record['Hospital'] = table_cells[1].text_content()
-            record['Region'] = table_cells[2].text_content()
-            record['Trolley total'] = table_cells[3].text_content()
-            record['Sales m'] = table_cells[4].text_content()
+            record['Racecourse'] = table_cells[0].text_content()
+            record['Address and Phone Number'] = table_cells[1].text_content()
+            #record['Region'] = table_cells[2].text_content()
+            #record['Trolley total'] = table_cells[3].text_content()
+            #record['Sales m'] = table_cells[4].text_content()
             
             # Print out the data we've gathered
             print record, '------------'
             # Finally, save the record to the datastore - 'Artist' is our unique key
-            scraperwiki.sqlite.save(["Hospital"], record)
+            scraperwiki.sqlite.save(["Racecourse"], record)
                             
 # scrape_and_look_for_next_link function: calls the scrape_table
 # function, then hunts for a 'next' link: if one is found, calls itself again
